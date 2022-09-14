1a102edb3e2631ea21e251173b1c3146f2e37f36
MartinY <martiny_3@yahoo.es>
2022-09-15 01:58:34 +0200

    Update PiTimeLapseGUI.py

--- PiTimeLapseGUI.py
+++ PiTimeLapseGUI.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 import logging
 import socket
 from tkinter import *
-from tkinter import messagebox, ttk
+from tkinter import messagebox, ttk, filedialog
 import threading
 from picamera import PiCamera, Color
 
@@ -84,21 +84,26 @@ def startTimeLapse():
     if strDurationCombo == 'hours':
         intDuration *= 60
 
+    strPicsPath = picsPath.cget("text")
+    if strPicsPath == '':
+        messagebox.showerror('ERROR: Path', 'Path undefined')
+        return
+
     isRunning = True
 
-    finalPath = PATH + '/timelapse_' + datetime.now().strftime('%Y%m%d')
+    finalPath = strPicsPath + '/timelapse_' + datetime.now().strftime('%Y%m%d')
     if not os.path.exists(finalPath):
         logging.info("Folder created: {}".format(finalPath))
         os.makedirs(finalPath)
 
-    lastPath.insert(0, (finalPath.split(PATH)[1]))
+    lastPath.config(text = (finalPath.split(strPicsPath)[1]))
 
     logging.info('Start TimeLapse')
 
     currentDateTime = datetime.now()
     logging.info("Start date:\t{}".format(currentDateTime))
-    startDateTime = currentDateTime + timedelta(seconds = 10)
-    startDateTime -= timedelta(seconds = startDateTime.second % 10, microseconds = startDateTime.microsecond)
+    startDateTime = currentDateTime + timedelta(seconds = 5)
+    startDateTime -= timedelta(seconds = startDateTime.second % 5, microseconds = startDateTime.microsecond)
     logging.info("Real start date:\t{}".format(startDateTime))
     timeToAdd = timedelta(minutes = intDuration)
     endDateTime = startDateTime + timeToAdd
@@ -135,15 +140,14 @@ def startTimeLapse():
         camera.capture(picFile)
 
         lastPicFile = picFile.split(finalPath)[1]
-        lastPic.delete(0, END)
-        lastPic.insert(0, lastPicFile)
+        lastPic.config(text = lastPicFile)
 
         # finish by FIN file
         endFile = "{}/FIN".format(finalPath)
         if (os.path.exists(endFile)):
             isRunning = False
-            lastPath.insert(0, '')
-            lastPic.insert(0, '')
+            lastPath.config(text = '')
+            lastPic.config(text = '')
             logging.info('Finish by FIN file')
             os.remove(endFile)
             break
@@ -152,8 +156,8 @@ def startTimeLapse():
         
         if mktime(endDateTime.timetuple()) <= nextTime:
             isRunning = False
-            lastPath.insert(0, '')
-            lastPic.insert(0, '')
+            lastPath.config(text = '')
+            lastPic.config(text = '')
             break
         
         sleepTime = nextTime - time()
@@ -163,8 +167,8 @@ def startTimeLapse():
     logging.info('Finish TimeLapse')
 
     isRunning = False
-    lastPath.delete(0, END)
-    lastPic.delete(0, END)
+    lastPath.config(text = '')
+    lastPic.config(text = '')
 
     return
 
@@ -201,6 +205,13 @@ def validateNumber(strNum):
     else:
         return False
 
+def selectPicsPath():
+    global picsPath
+
+    selectedPicsPath = filedialog.askdirectory(initialdir = PATH, title = "Select folder")
+    picsPath.config(text = selectedPicsPath)
+    logging.info(f"Selected path: {selectedPicsPath}")
+
 #codePath = os.path.dirname(__file__)
 codePath = os.path.dirname(os.path.realpath(__file__))
 iconPath = codePath + '/camera_small.png'
@@ -222,32 +233,36 @@ menubar.add_cascade(label = "File", menu = filemenu)
 
 validateNumberReg = mainWindow.register(validateNumber)
 
-Label(mainWindow, text = 'Duration:').grid(row = 0, column = 0, sticky = W)
+Button(mainWindow, text = 'Path:', command = selectPicsPath, width = 15, anchor = W).grid(row = 0, column = 0, sticky = W)
+picsPath = Label(mainWindow, text = PATH, relief = 'sunken', width = 17, anchor = W)
+picsPath.grid(row = 0, column = 1, sticky = W)
+
+Label(mainWindow, text = 'Duration:').grid(row = 1, column = 0, sticky = W)
 duration = Entry(mainWindow, width = 5)
 duration.insert(0, '1')
-duration.grid(row = 0, column = 1, sticky = W)
+duration.grid(row = 1, column = 1, sticky = W)
 duration.config(validate = 'key', validatecommand = (validateNumberReg, '%P'))
 durationCombo = ttk.Combobox(state = 'readonly', values = ['minutes', 'hours'], width = 10)
-durationCombo.grid(row = 0, column = 1, sticky = E)
+durationCombo.grid(row = 1, column = 1, sticky = E)
 durationCombo.current(0)
 
-Label(mainWindow, text = 'Period(s):').grid(row = 1, column = 0, sticky = W)
+Label(mainWindow, text = 'Period(s):').grid(row = 2, column = 0, sticky = W)
 period = Entry(mainWindow, width = 5)
 period.insert(0, '15')
-period.grid(row = 1, column = 1, sticky = W)
+period.grid(row = 2, column = 1, sticky = W)
 period.config(validate = 'key', validatecommand = (validateNumberReg, '%P'))
 
-Button(mainWindow, text = 'Start', command = lambda: threading.Thread(target = startTimeLapse).start(), width = 15).grid(row = 2, column = 0)
-Button(mainWindow, text = 'Stop', command = lambda: threading.Thread(target = stopTimeLapse).start(), width = 15).grid(row = 2, column = 1)
+Button(mainWindow, text = 'Start', command = lambda: threading.Thread(target = startTimeLapse).start(), width = 15).grid(row = 3, column = 0)
+Button(mainWindow, text = 'Stop', command = lambda: threading.Thread(target = stopTimeLapse).start(), width = 15).grid(row = 3, column = 1)
 
-Label(mainWindow, text = 'TimeLapse Path:').grid(row = 3, column = 0, sticky = W)
-lastPath = Entry(mainWindow, width = 17)
-lastPath.grid(row = 3, column = 1, sticky = W)
-Label(mainWindow, text = 'Last Pic:').grid(row = 4, column = 0, sticky = W)
-lastPic = Entry(mainWindow, width = 17)
-lastPic.grid(row = 4, column = 1, sticky = W)
+Label(mainWindow, text = 'TimeLapse Path:').grid(row = 4, column = 0, sticky = W)
+lastPath = Label(mainWindow, text = '', relief = 'sunken', width = 17, anchor = W)
+lastPath.grid(row = 4, column = 1, sticky = W)
+Label(mainWindow, text = 'Last Pic:').grid(row = 5, column = 0, sticky = W)
+lastPic = Label(mainWindow, text = '', relief = 'sunken', width = 17, anchor = W)
+lastPic.grid(row = 5, column = 1, sticky = W)
 
-Button(mainWindow, text = 'Exit', command = exitTimeLapse, width = 15).grid(row = 5, column = 0, columnspan = 2)
+Button(mainWindow, text = 'Exit', command = exitTimeLapse, width = 15).grid(row = 6, column = 0, columnspan = 2)
 
 #mainWindow.after(timeTillDestroy*1000, lambda: mainWindow.destroy()) # Destroy the widget after (time) seconds
 mainWindow.mainloop()
